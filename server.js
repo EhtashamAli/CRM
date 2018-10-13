9f688a1dfda568c22cec9c6308e4d8e46e837b99
EhtashamAli <ehtasham.ali1979@gmail.com>
2018-10-14 03:05:35 +0500

    ImageSetup

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const app = require('./app').app;
+
+const port = process.env.PORT || 3000;
+
+var server = http.createServer(app);
+
+server.listen(port , () => {
+  console.log(`server is up and running on port: ${port}`);
+});
