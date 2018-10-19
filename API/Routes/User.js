93dd3271044478951570bbed79cca49b1a74c29a
EhtashamAli <ehtasham.ali1979@gmail.com>
2018-10-20 02:32:00 +0500

    before deployment

--- API/Routes/User.js
+++ API/Routes/User.js
@@ -22,116 +22,164 @@ router.post('/update:id' , (req , res) => {
     });
 });
 
-module.exports = router;
-// //   m5kOdvy00Hu2MEI1HoM3
-// router.post('/login' , (req , res) => {
 
-//     let user = [];
-//     let Password = ''; //declare array for retrieving documents
-//     USER.where('Email','==', req.body.email)
-//     .onSnapshot(querySnapshot=> {
-//         querySnapshot.forEach((doc) => {
-//             // binded to the UI
-//             user.push(doc.id);
-//             Password = doc.data().Password;
-//         });
-//         if(user.length <= 0){
-//             res.status(404).json({
-//                 result: "Email or password is incorrect"
-//             });  
-//         } else {
-//             bcrypt.compare(req.body.password, Password)
-//             .then(isMatch => {
-//               if (isMatch) {
-//                 res.status(200).json({
-//                             id: user[0]
-//                         });
-//               } else {
-//                 res.status(404).json({
-//                   status: 404,
-//                   message: "Email or password is incorrect"
-//                 })
-//               }
-//             })
-//             .catch(err => {
-//               res.status(500).json({
-//                 status: "Something Went Wrong",
-//                 Error: {
-//                   Message: err
-//                 }
-//               });
-//             });
-//         }
-//     },(error)=>{
-//             res.status(500).json({
-//                 error: error
-//             })
-//     });
-// });
-// router.post('/register' , (req , res) => {
+//   m5kOdvy00Hu2MEI1HoM3
+router.post('/login' , (req , res) => {
 
-//     const {
-//         errors,
-//         isValid
-//       } = validateRegisterInput(req.body);
+    let user = [];
+    let Password = ''; //declare array for retrieving documents
+    USER.where('Email','==', req.body.email)
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(doc.data());
+            user.push(doc.id);
+            Password = doc.data().Password;
+        });
+        if(user.length <= 0){
+            res.status(404).json({
+                result: "1Email or password is incorrect"
+            }); 
+        } else {
+            bcrypt.compare(req.body.password, Password)
+            .then(isMatch => {
+              if (isMatch) {
+                res.status(200).json({
+                            id: user[0]
+                        });
+              } else {
+                res.status(404).json({
+                  status: 404,
+                  message: "Email or password is incorrect"
+                })
+              }
+            })
+            .catch(err => {
+              res.status(500).json({
+                status: "Something Went Wrong",
+                Error: {
+                  Message: err
+                }
+              });
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            status: "Something Went Wrong",
+            Error: {
+              Message: err
+            }
+          });
+    })
+});
+router.post('/register' , (req , res) => {
 
-//       if (!isValid) {
-//         return res.status(500).json(errors);
-//       }
+    const {
+        errors,
+        isValid
+      } = validateRegisterInput(req.body);
 
-//     const userExist = [];
-//     USER.where('Email','==', req.body.email)
-//     .get()
-//     .then(function(querySnapshot) {
-//         querySnapshot.forEach(function(doc) {
-//             userExist.push(doc.data().Email);
-//         });
-//         if(userExist.length > 0){
-//             console.log(userExist);
-//              res.status(500).json({
-//                 result: "Email Already Exists"
-//             });  
-//         } else {
-//             console.log('else');
-//             const User = {
-//                 UserName : req.body.name,
-//                 Password : req.body.password,
-//                 Email : req.body.email
-//             }
-//              bcrypt.genSalt(10, function (err, salt) {
-//                 bcrypt.hash(User.Password, salt, function (err, hash) {
-//                   // if (err) throw err;
-//                   User.Password = hash
+      if (!isValid) {
+        return res.status(500).json(errors);
+      }
 
-//                   USER.doc()
-//                   .set(JSON.parse(JSON.stringify(User)))
-//                   .then(result => {
-//                        res.status(200).json({
-//                           result : User
-//                       });
-//                   })
-//                   .catch(err => {
-//                     res.status(500).json({
-//                        err
-//                     });    
-//                   });
-//                 });
-//               });
-//         }
-//     })
-//     .catch(function(error) {
-//         console.log("Error getting documents: ", error);
-//     });
+    const userExist = [];
+    USER.where('Email','==', req.body.email)
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            userExist.push(doc.data().Email);
+        });
+        if(userExist.length > 0){
+            console.log(userExist);
+             res.status(500).json({
+                result: "Email Already Exists"
+            });  
+        } else {
+            console.log('else');
+            const User = {
+                UserName : req.body.name,
+                Password : req.body.password,
+                Email : req.body.email
+            }
+             bcrypt.genSalt(10, function (err, salt) {
+                bcrypt.hash(User.Password, salt, function (err, hash) {
+                  // if (err) throw err;
+                  User.Password = hash
 
-//     //  USER.where('Email','==', req.body.email)
-//     // .onSnapshot(querySnapshot=> {
-//     //     querySnapshot.forEach((doc) => {
-//     //         userExist.push(doc.data().Email);
-//     //     });
+                  USER.doc()
+                  .set(JSON.parse(JSON.stringify(User)))
+                  .then(result => {
+                       res.status(200).json({
+                          result : User
+                      });
+                  })
+                  .catch(err => {
+                    res.status(500).json({
+                       err
+                    });    
+                  });
+                });
+              });
+        }
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+
+    //  USER.where('Email','==', req.body.email)
+    // .onSnapshot(querySnapshot=> {
+    //     querySnapshot.forEach((doc) => {
+    //         userExist.push(doc.data().Email);
+    //     });
         
-//     //     },(error)=>{
-//     //         res.status(500).json({
-//     //             error
-//     //         });   
-//     //     });             
+    //     },(error)=>{
+    //         res.status(500).json({
+    //             error
+    //         });   
+    //     });             
+});
+
+
+module.exports = router;
+
+// USER.where('Email','==', req.body.email)
+// .onSnapshot(querySnapshot=> {
+//     querySnapshot.forEach((doc) => {
+//         console.log(doc.id);
+//         user.push(doc.id);
+//         Password = doc.data().Password;
+//     });
+//     if(user.length <= 0){
+//         res.status(404).json({
+//             result: "1Email or password is incorrect"
+//         });  
+//     } else {
+//         bcrypt.compare(req.body.password, Password)
+//         .then(isMatch => {
+//           if (isMatch) {
+//             res.status(200).json({
+//                         id: user[0]
+//                     });
+//           } else {
+//             res.status(404).json({
+//               status: 404,
+//               message: "Email or password is incorrect"
+//             })
+//           }
+//         })
+//         .catch(err => {
+//           res.status(500).json({
+//             status: "Something Went Wrong",
+//             Error: {
+//               Message: err
+//             }
+//           });
+//         });
+//     }
+// },(error)=>{
+//         res.status(500).json({
+//             error: error
+//         })
 // });
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const app = require('./app');
+const app = require('./application');
 
 const port = process.env.PORT || 3000;
 
